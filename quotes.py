712d672891f9bb8252aaf413bbe8f9e313160363
pevescarol <carol.peves@gmail.com>
2023-02-15 08:34:12 -0300

    add database configuration, endpoints

--- quotes.py
+++ quotes.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://caro:Benito89@localhost/quotes2'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db = SQLAlchemy(app)
+
+class Favquotes2(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30))
+    quote = db.Column(db.String(2000))
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    result = Favquotes2.query.all()
+    return render_template('index.html', result=result)
+
 
 @app.route('/quotes')
 def quotes():
     return render_template('quotes.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    author = request.form['author']
+    quote = request.form['quote']
+    quotedata = Favquotes2(author=author, quote=quote)
+    db.session.add(quotedata)
+    db.session.commit()
+    
+    return redirect(url_for('index'))
